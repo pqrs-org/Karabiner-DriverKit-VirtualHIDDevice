8162f0317f254c591e503224dbcfbf3a01bc8d27
Takayama Fumihiko <tekezo@pqrs.org>
2020-05-13 21:07:07 +0900

    scripts/update-info-plist.sh -> scripts/update-version.sh

--- src/scripts/update-version.sh
+++ src/scripts/update-version.sh
@@ -4,7 +4,7 @@ basedir=$(dirname $0)
 
 version=$(cat $basedir/../../version)
 
-for f in $(find $basedir/.. -name 'Info.plist.in'); do
+for f in $(find $basedir/.. -name 'Info.plist.in' -o -name 'version.hpp.in'); do
     outfile=$(dirname $f)/$(basename $f .in)
     sed "s|@VERSION@|$version|g" $f >$outfile.tmp
     if cmp -s $outfile.tmp $outfile; then
