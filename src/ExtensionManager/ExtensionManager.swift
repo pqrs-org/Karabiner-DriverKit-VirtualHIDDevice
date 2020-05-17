588e7750ad6811a1d97265a9ba65617639143fc5
Takayama Fumihiko <tekezo@pqrs.org>
2020-05-17 17:21:09 +0900

    add ExtensionManager.stateChanged

--- src/ExtensionManager/ExtensionManager.swift
+++ src/ExtensionManager/ExtensionManager.swift
@@ -1,58 +1,153 @@
 import Foundation
-import os.log
 import SystemExtensions
 
 class ExtensionManager: NSObject, OSSystemExtensionRequestDelegate {
     static let shared = ExtensionManager()
-    let driverIdentifier = "org.pqrs.driverkit.KarabinerDriverKitVirtualHIDKeyboard"
 
-    func activate() {
-        os_log("%{public}@ activate", driverIdentifier)
+    //
+    // Notification names
+    //
 
-        let request = OSSystemExtensionRequest.activationRequest(
-            forExtensionWithIdentifier: driverIdentifier,
-            queue: .main
-        )
-        request.delegate = self
+    static let stateChanged = Notification.Name("stateChanged")
 
-        OSSystemExtensionManager.shared.submitRequest(request)
+    public enum State {
+        case activationRequested
+        case deactivationRequested
+        case requestFinished
+        case requestFailed
+        case replacingCanceled
+        case willReplace
+        case userApprovalRequired
+        case rebootRequired
     }
 
-    func deactivate() {
-        os_log("%{public}@ deactivate", driverIdentifier)
+    struct NotificationObject {
+        let bundleIdentifier: String
+        let state: State
+        let message: String
+    }
 
-        let request = OSSystemExtensionRequest.deactivationRequest(
-            forExtensionWithIdentifier: driverIdentifier,
+    func activate(_ bundleIdentifier: String) {
+        let request = OSSystemExtensionRequest.activationRequest(
+            forExtensionWithIdentifier: bundleIdentifier,
             queue: .main
         )
         request.delegate = self
 
         OSSystemExtensionManager.shared.submitRequest(request)
+
+        NotificationCenter.default.post(
+            name: ExtensionManager.stateChanged,
+            object: NotificationObject(
+                bundleIdentifier: bundleIdentifier,
+                state: .activationRequested,
+                message: "activation of \(bundleIdentifier) is requested"
+            )
+        )
+    }
+
+    func deactivate(_ bundleIdentifier: String) {
+        let request = OSSystemExtensionRequest.deactivationRequest(
+            forExtensionWithIdentifier: bundleIdentifier,
+            queue: .main
+        )
+        request.delegate = self
+
+        OSSystemExtensionManager.shared.submitRequest(request)
+
+        NotificationCenter.default.post(
+            name: ExtensionManager.stateChanged,
+            object: NotificationObject(
+                bundleIdentifier: bundleIdentifier,
+                state: .deactivationRequested,
+                message: "deactivation of \(bundleIdentifier) is requested"
+            )
+        )
     }
 
     //
     // OSSystemExtensionRequestDelegate
     //
 
-    func request(_: OSSystemExtensionRequest,
+    func request(_ request: OSSystemExtensionRequest,
                  didFinishWithResult result: OSSystemExtensionRequest.Result) {
-        os_log("ExtensionManager request didFinishWithResult:%{public}@", result.rawValue)
+        NotificationCenter.default.post(
+            name: ExtensionManager.stateChanged,
+            object: NotificationObject(
+                bundleIdentifier: request.identifier,
+                state: .requestFinished,
+                message: "request of \(request.identifier) is finished"
+            )
+        )
+
+        switch result {
+        case .completed:
+            break
+        case .willCompleteAfterReboot:
+            NotificationCenter.default.post(
+                name: ExtensionManager.stateChanged,
+                object: NotificationObject(
+                    bundleIdentifier: request.identifier,
+                    state: .rebootRequired,
+                    message: "request of \(request.identifier) requires reboot"
+                )
+            )
+        @unknown default:
+            break
+        }
     }
 
-    func request(_: OSSystemExtensionRequest,
+    func request(_ request: OSSystemExtensionRequest,
                  didFailWithError error: Error) {
-        os_log("ExtensionManager request didFailWithError:%{public}@", error.localizedDescription)
+        NotificationCenter.default.post(
+            name: ExtensionManager.stateChanged,
+            object: NotificationObject(
+                bundleIdentifier: request.identifier,
+                state: .requestFailed,
+                message: "request of \(request.identifier) is failed with error: \(error.localizedDescription)"
+            )
+        )
     }
 
-    func requestNeedsUserApproval(_: OSSystemExtensionRequest) {
-        os_log("ExtensionManager requestNeedsUserApproval")
+    func requestNeedsUserApproval(_ request: OSSystemExtensionRequest) {
+        NotificationCenter.default.post(
+            name: ExtensionManager.stateChanged,
+            object: NotificationObject(
+                bundleIdentifier: request.identifier,
+                state: .userApprovalRequired,
+                message: "request of \(request.identifier) requires user approval"
+            )
+        )
     }
 
-    func request(_: OSSystemExtensionRequest,
+    func request(_ request: OSSystemExtensionRequest,
                  actionForReplacingExtension existing: OSSystemExtensionProperties,
                  withExtension ext: OSSystemExtensionProperties) -> OSSystemExtensionRequest.ReplacementAction {
-        os_log("ExtensionManager request actionForReplacingExtension:%{public}@ withExtension:%{public}@", existing, ext)
+        let existingVersion = existing.bundleVersion
+        let extVersion = ext.bundleVersion
 
-        return .replace
+        if extVersion.compare(existingVersion, options: .numeric) == .orderedAscending {
+            NotificationCenter.default.post(
+                name: ExtensionManager.stateChanged,
+                object: NotificationObject(
+                    bundleIdentifier: request.identifier,
+                    state: .willReplace,
+                    message: "\(request.identifier) will replace to \(extVersion) from \(existingVersion)"
+                )
+            )
+
+            return .replace
+        }
+
+        NotificationCenter.default.post(
+            name: ExtensionManager.stateChanged,
+            object: NotificationObject(
+                bundleIdentifier: request.identifier,
+                state: .replacingCanceled,
+                message: "request of \(request.identifier) is canceled"
+            )
+        )
+
+        return .cancel
     }
 }
