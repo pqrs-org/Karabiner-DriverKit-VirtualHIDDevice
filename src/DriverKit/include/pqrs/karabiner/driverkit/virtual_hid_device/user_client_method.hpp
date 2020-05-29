e2fe7ce76174be66666c75a71e7b782a87d6911f
Takayama Fumihiko <tekezo@pqrs.org>
2020-05-30 00:34:54 +0900

    update src/DriverKit/include

---
+++ src/DriverKit/include/pqrs/karabiner/driverkit/virtual_hid_device/user_client_method.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace pqrs {
+namespace karabiner {
+namespace driverkit {
+namespace virtual_hid_device {
+
+enum class user_client_method {
+  //
+  // keyboard
+  //
+
+  post_keyboard_input_report,
+  post_consumer_input_report,
+  post_apple_vendor_top_case_input_report,
+  post_apple_vendor_keyboard_input_report,
+  reset_virtual_hid_keyboard,
+
+  //
+  // pointing
+  //
+
+  post_pointing_input_report,
+  reset_virtual_hid_pointing,
+
+  end_,
+};
+
+} // namespace virtual_hid_device
+} // namespace driverkit
+} // namespace karabiner
+} // namespace pqrs
