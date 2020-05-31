148cf3e1ab9bd25a7faf0abd8acf29dd176b3b01
Takayama Fumihiko <tekezo@pqrs.org>
2020-05-31 13:27:32 +0900

    update client

--- client-examples/virtual-keyboard-client/main.cpp
+++ client-examples/virtual-keyboard-client/main.cpp
@@ -9,178 +9,26 @@
 #include <IOKit/hidsystem/ev_keymap.h>
 #include <cmath>
 #include <iostream>
+#include <pqrs/hid.hpp>
 #include <pqrs/karabiner/driverkit/virtual_hid_keyboard_client.hpp>
 #include <pqrs/osx/iokit_return.hpp>
-#include <pqrs/osx/kern_return.hpp>
 #include <thread>
 
 int main(int argc, const char* argv[]) {
-#if 0
-  if (getuid() != 0) {
-    std::cerr << "dispatch_keyboard_event_example requires root privilege." << std::endl;
-  }
-#endif
+  auto client = std::make_shared<pqrs::karabiner::driverkit::virtual_hid_keyboard_client>();
 
-  auto client = std::make_shared<pqrs::karabiner::driverkit::virtual_hid_keyboard_client>;
+  if (!client->connected()) {
+    std::cerr << "virtual_hid_keyboard is not connected: " << std::endl;
+  } else {
+    pqrs::karabiner::driverkit::virtual_hid_device::hid_report::keyboard_input report;
 
-#if 0
-  {
-    pqrs::karabiner_virtual_hid_device::properties::keyboard_initialization properties;
-    kr = pqrs::karabiner_virtual_hid_device_methods::initialize_virtual_hid_keyboard(connect, properties);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "initialize_virtual_hid_keyboard error" << std::endl;
+    report.modifiers.insert(pqrs::karabiner::driverkit::virtual_hid_device::hid_report::modifier::left_command);
+    report.keys.insert(type_safe::get(pqrs::hid::usage::keyboard_or_keypad::keyboard_tab));
+
+    pqrs::osx::iokit_return kr = client->post_keyboard_input_report(report);
+    if (!kr) {
+      std::cerr << "post_keyboard_input_report error: " << kr << std::endl;
     }
-
-    while (true) {
-      std::cout << "Checking virtual_hid_keyboard is ready..." << std::endl;
-
-      bool ready;
-      kr = pqrs::karabiner_virtual_hid_device_methods::is_virtual_hid_keyboard_ready(connect, ready);
-      if (kr != KERN_SUCCESS) {
-        std::cerr << "is_virtual_hid_keyboard_ready error: " << kr << std::endl;
-      } else {
-        if (ready) {
-          std::cout << "virtual_hid_keyboard is ready." << std::endl;
-          break;
-        }
-      }
-
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-  }
-
-  // keyboard_input
-
-  {
-    pqrs::karabiner_virtual_hid_device::properties::keyboard_initialization properties;
-#if 0
-    properties.country_code = 33;
-#endif
-    kr = pqrs::karabiner_virtual_hid_device_methods::initialize_virtual_hid_keyboard(connect, properties);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "initialize_virtual_hid_keyboard error" << std::endl;
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  }
-
-  {
-    pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input report;
-    for (int i = 0; i < 10; ++i) {
-      uint8_t key = 0;
-      switch (i % 5) {
-        case 0:
-          key = kHIDUsage_KeyboardA;
-          break;
-        case 1:
-          key = kHIDUsage_KeyboardB;
-          break;
-        case 2:
-          key = kHIDUsage_KeyboardC;
-          break;
-        case 3:
-          key = kHIDUsage_KeyboardD;
-          report.modifiers.insert(pqrs::karabiner_virtual_hid_device::hid_report::modifier::right_shift);
-          break;
-        case 4:
-          key = kHIDUsage_KeyboardE;
-          break;
-      }
-
-      if (report.keys.exists(key)) {
-        report.keys.erase(key);
-      } else {
-        report.keys.insert(key);
-      }
-
-      kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
-      if (kr != KERN_SUCCESS) {
-        std::cerr << "post_keyboard_input_report error" << std::endl;
-      }
-
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-  }
-
-  // consumer_input (option+mute)
-
-  {
-    pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input report;
-    report.modifiers.insert(pqrs::karabiner_virtual_hid_device::hid_report::modifier::left_option);
-
-    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "post_keyboard_input_report error" << std::endl;
-    }
-  }
-  {
-    pqrs::karabiner_virtual_hid_device::hid_report::consumer_input report;
-    report.keys.insert(kHIDUsage_Csmr_Mute);
-
-    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "post_keyboard_input_report error" << std::endl;
-    }
-  }
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-  // apple_vendor_top_case_input (fn+spacebar)
-
-  {
-    pqrs::karabiner_virtual_hid_device::hid_report::apple_vendor_top_case_input report;
-    report.keys.insert(0x03); // kHIDUsage_AV_TopCase_KeyboardFn
-
-    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "post_keyboard_input_report error" << std::endl;
-    }
-  }
-  {
-    pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input report;
-    report.keys.insert(kHIDUsage_KeyboardSpacebar);
-
-    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "post_keyboard_input_report error" << std::endl;
-    }
-  }
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-  // apple_vendor_keyboard_input (command+mission_control)
-
-  {
-    pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input report;
-    report.modifiers.insert(pqrs::karabiner_virtual_hid_device::hid_report::modifier::left_command);
-
-    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "post_keyboard_input_report error" << std::endl;
-    }
-  }
-
-  {
-    pqrs::karabiner_virtual_hid_device::hid_report::apple_vendor_keyboard_input report;
-    report.keys.insert(0x0010); // kHIDUsage_AppleVendorKeyboard_Expose_All
-
-    kr = pqrs::karabiner_virtual_hid_device_methods::post_keyboard_input_report(connect, report);
-    if (kr != KERN_SUCCESS) {
-      std::cerr << "post_keyboard_input_report error" << std::endl;
-    }
-  }
-
-  kr = pqrs::karabiner_virtual_hid_device_methods::reset_virtual_hid_keyboard(connect);
-  if (kr != KERN_SUCCESS) {
-    std::cerr << "reset_virtual_hid_keyboard error" << std::endl;
-  }
-
-#endif
-
-  std::cout << std::endl;
-  std::cout << "press control-c to quit" << std::endl;
-  while (true) {
-    std::cout << "." << std::flush;
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 
   client = nullptr;
---
+++ client-examples/virtual-keyboard-client/vendor/cget/pkg/pqrs-org__cpp-hid/install/include/pqrs/hid/product_id.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// (C) Copyright Takayama Fumihiko 2020.
+// Distributed under the Boost Software License, Version 1.0.
+// (See http://www.boost.org/LICENSE_1_0.txt)
+
+#include <functional>
+#include <iostream>
+#include <type_safe/strong_typedef.hpp>
+
+namespace pqrs {
+namespace hid {
+namespace product_id {
+struct value_t : type_safe::strong_typedef<value_t, uint64_t>,
+                 type_safe::strong_typedef_op::equality_comparison<value_t>,
+                 type_safe::strong_typedef_op::relational_comparison<value_t> {
+  using strong_typedef::strong_typedef;
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const value_t& value) {
+  return stream << type_safe::get(value);
+}
+} // namespace product_id
+} // namespace hid
+} // namespace pqrs
+
+namespace std {
+template <>
+struct hash<pqrs::hid::product_id::value_t> : type_safe::hashable<pqrs::hid::product_id::value_t> {
+};
+} // namespace std
