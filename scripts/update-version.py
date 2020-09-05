4bb978efba12fb6eefc190168940de31c84e5d9d
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-05 23:05:06 +0900

    Add @VERSION_NUMBER@, @DRIVER_VERSION_NUMBER@

--- scripts/update-version.py
+++ scripts/update-version.py
@@ -10,7 +10,14 @@ topDirectory = Path(__file__).resolve(True).parents[1]
 
 with topDirectory.joinpath('version').open() as versionFile, topDirectory.joinpath('driver-version').open() as driverVersionFile:
     version = versionFile.readline().strip()
+    versionNumber = 0
+    for v in version.split('.'):
+        versionNumber = versionNumber * 100 + int(v)
+
     driverVersion = driverVersionFile.readline().strip()
+    driverVersionNumber = 0
+    for v in driverVersion.split('.'):
+        driverVersionNumber = driverVersionNumber * 100 + int(v)
 
     for templateFilePath in chain(topDirectory.rglob('*.hpp.in'),
                                   topDirectory.rglob('*.plist.in'),
@@ -33,7 +40,9 @@ with topDirectory.joinpath('version').open() as versionFile, topDirectory.joinpa
             for index, templateLine in enumerate(templateLines):
                 line = templateLine
                 line = line.replace('@VERSION@', version)
+                line = line.replace('@VERSION_NUMBER@', str(versionNumber))
                 line = line.replace('@DRIVER_VERSION@', driverVersion)
+                line = line.replace('@DRIVER_VERSION_NUMBER@', str(driverVersionNumber))
 
                 if (replacedLines[index] != line):
                     needsUpdate = True
