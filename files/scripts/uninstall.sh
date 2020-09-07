1f2186298807c283570800ee3344483fec0e3160
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-07 23:12:28 +0900

    Remove uninstalled dialog

--- files/scripts/uninstall.sh
+++ files/scripts/uninstall.sh
@@ -13,6 +13,4 @@
 
 /bin/bash '/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/scripts/uninstall_core.sh'
 
-/usr/bin/osascript -e 'display dialog "Karabiner-DriverKit-VirtualHIDDevice has been uninstalled.\nPlease restart your system." buttons {"OK"}'
-
 exit 0
