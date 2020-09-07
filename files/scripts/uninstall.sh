c8812c09f4b4aea00ceee3f2a3da60ef05070c64
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-07 16:24:10 +0900

    Deactivate driver at uninstall.sh

--- files/scripts/uninstall.sh
+++ files/scripts/uninstall.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#
+# Deactivate extension
+#
+
+/usr/bin/osascript -e 'display dialog "Removing Karabiner VirtualHIDDevice driver.\nThe administrator password will be required to complete." buttons {"OK"}'
+/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager deactivate
+
+#
+# uninstall_core.sh
+#
+
 /bin/bash '/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/scripts/uninstall_core.sh'
 
 /usr/bin/osascript -e 'display dialog "Karabiner-DriverKit-VirtualHIDDevice has been uninstalled.\nPlease restart your system." buttons {"OK"}'
