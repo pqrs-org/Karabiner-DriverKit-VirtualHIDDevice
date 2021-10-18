84be4d936cc10693c012c2d7f58372b6edd1654d
Takayama Fumihiko <tekezo@pqrs.org>
2021-10-18 22:09:05 +0900

    Use snake case

--- scripts/update-version.py
+++ scripts/update-version.py
@@ -6,50 +6,51 @@ import sys
 from pathlib import Path
 from itertools import chain
 
-topDirectory = Path(__file__).resolve(True).parents[1]
+top_directory = Path(__file__).resolve(True).parents[1]
 
-with topDirectory.joinpath('version').open() as versionFile, topDirectory.joinpath('driver-version').open() as driverVersionFile:
-    version = versionFile.readline().strip()
-    versionNumber = 0
+with top_directory.joinpath('version').open() as version_file, top_directory.joinpath('driver-version').open() as driver_version_file:
+    version = version_file.readline().strip()
+    version_number = 0
     for v in version.split('.'):
-        versionNumber = versionNumber * 100 + int(v)
+        version_number = version_number * 100 + int(v)
 
-    driverVersion = driverVersionFile.readline().strip()
-    driverVersionNumber = 0
-    for v in driverVersion.split('.'):
-        driverVersionNumber = driverVersionNumber * 100 + int(v)
+    driver_version = driver_version_file.readline().strip()
+    driver_version_number = 0
+    for v in driver_version.split('.'):
+        driver_version_number = driver_version_number * 100 + int(v)
 
-    for templateFilePath in chain(topDirectory.rglob('*.hpp.in'),
-                                  topDirectory.rglob('*.plist.in'),
-                                  topDirectory.rglob('*.xml.in')):
-        replacedFilePath = Path(re.sub(r'\.in$', '', str(templateFilePath)))
-        needsUpdate = False
+    for template_file_path in chain(top_directory.rglob('*.hpp.in'),
+                                    top_directory.rglob('*.plist.in'),
+                                    top_directory.rglob('*.xml.in')):
+        replaced_file_path = Path(
+            re.sub(r'\.in$', '', str(template_file_path)))
+        needs_update = False
 
-        with templateFilePath.open('r') as templateFile:
-            templateLines = templateFile.readlines()
-            replacedLines = []
+        with template_file_path.open('r') as template_file:
+            template_lines = template_file.readlines()
+            replaced_lines = []
 
-            if replacedFilePath.exists():
-                with replacedFilePath.open('r') as replacedFile:
-                    replacedLines = replacedFile.readlines()
-                    while len(replacedLines) < len(templateLines):
-                        replacedLines.append('')
+            if replaced_file_path.exists():
+                with replaced_file_path.open('r') as replaced_file:
+                    replaced_lines = replaced_file.readlines()
+                    while len(replaced_lines) < len(template_lines):
+                        replaced_lines.append('')
             else:
-                replacedLines = templateLines
+                replaced_lines = template_lines
 
-            for index, templateLine in enumerate(templateLines):
-                line = templateLine
+            for index, template_line in enumerate(template_lines):
+                line = template_line
                 line = line.replace('@VERSION@', version)
-                line = line.replace('@VERSION_NUMBER@', str(versionNumber))
-                line = line.replace('@DRIVER_VERSION@', driverVersion)
+                line = line.replace('@VERSION_NUMBER@', str(version_number))
+                line = line.replace('@DRIVER_VERSION@', driver_version)
                 line = line.replace('@DRIVER_VERSION_NUMBER@',
-                                    str(driverVersionNumber))
+                                    str(driver_version_number))
 
-                if (replacedLines[index] != line):
-                    needsUpdate = True
-                    replacedLines[index] = line
+                if (replaced_lines[index] != line):
+                    needs_update = True
+                    replaced_lines[index] = line
 
-        if needsUpdate:
-            with replacedFilePath.open('w') as replacedFile:
-                print("Update " + str(replacedFilePath))
-                replacedFile.write(''.join(replacedLines))
+        if needs_update:
+            with replaced_file_path.open('w') as replaced_file:
+                print("Update " + str(replaced_file_path))
+                replaced_file.write(''.join(replaced_lines))
