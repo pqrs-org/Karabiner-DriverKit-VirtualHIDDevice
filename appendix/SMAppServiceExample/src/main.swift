df0060f17849dfd1f3d78d40df2edf1dd9c71a39
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-14 23:27:18 +0900

    Update SMAppServiceExample

--- appendix/SMAppServiceExample/src/main.swift
+++ appendix/SMAppServiceExample/src/main.swift
@@ -1,45 +1,53 @@
 import Foundation
 import ServiceManagement
 
-let daemonPlistName = "org.pqrs.Karabiner-DriverKit-SMAppServiceExample.plist"
+let daemonPlistNames = [
+  "org.pqrs.Karabiner-DriverKit-SMAppServiceExample.plist"
+]
 
 RunLoop.main.perform {
   for argument in CommandLine.arguments {
     if argument == "register" {
-      let s = SMAppService.daemon(plistName: daemonPlistName)
-      do {
-        try s.register()
-        print("Successfully registered \(s)")
-        exit(0)
-      } catch {
-        print("Unable to register \(error)")
-        exit(1)
+      for daemonPlistName in daemonPlistNames {
+        let s = SMAppService.daemon(plistName: daemonPlistName)
+        do {
+          try s.register()
+          print("Successfully registered \(s)")
+        } catch {
+          print("Unable to register \(error)")
+          exit(1)
+        }
       }
+      exit(0)
 
     } else if argument == "unregister" {
-      let s = SMAppService.daemon(plistName: daemonPlistName)
-      do {
-        try s.unregister()
-        print("Successfully unregistered \(s)")
-        exit(0)
-      } catch {
-        print("Unable to unregister \(error)")
-        exit(1)
+      for daemonPlistName in daemonPlistNames {
+        let s = SMAppService.daemon(plistName: daemonPlistName)
+        do {
+          try s.unregister()
+          print("Successfully unregistered \(s)")
+        } catch {
+          print("Unable to unregister \(error)")
+          exit(1)
+        }
       }
+      exit(0)
 
     } else if argument == "status" {
-      let s = SMAppService.daemon(plistName: daemonPlistName)
-      switch s.status {
-      case .notRegistered:
-        print("notRegistered")
-      case .enabled:
-        print("enabled")
-      case .requiresApproval:
-        print("requiresApproval")
-      case .notFound:
-        print("notFound")
-      @unknown default:
-        print("unknown \(s.status)")
+      for daemonPlistName in daemonPlistNames {
+        let s = SMAppService.daemon(plistName: daemonPlistName)
+        switch s.status {
+        case .notRegistered:
+          print("\(s) notRegistered")
+        case .enabled:
+          print("\(s) enabled")
+        case .requiresApproval:
+          print("\(s) requiresApproval")
+        case .notFound:
+          print("\(s) notFound")
+        @unknown default:
+          print("\(s) unknown \(s.status)")
+        }
       }
 
       exit(0)
