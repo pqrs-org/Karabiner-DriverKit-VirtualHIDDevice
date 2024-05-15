f009968599e89bb4c21055dccd7fe451391fa9ef
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-15 23:03:57 +0900

    Kill Karabiner-VirtualHIDDevice-Daemon in remove_files.sh

--- files/scripts/uninstall/remove_files.sh
+++ files/scripts/uninstall/remove_files.sh
@@ -16,6 +16,12 @@ if [ -f /Library/LaunchDaemons/org.pqrs.Karabiner-DriverKit-VirtualHIDDeviceClie
 fi
 
 #
+# Kill processes
+#
+
+killall Karabiner-VirtualHIDDevice-Daemon
+
+#
 # Kill processes in previous versions
 #
 
