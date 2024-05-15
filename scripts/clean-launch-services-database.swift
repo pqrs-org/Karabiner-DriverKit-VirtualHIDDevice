2f08ccc37ac33de57b97a9ef10df6faeea2def32
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-15 23:23:57 +0900

    Karabiner-DriverKit-SMAppServiceExample.app -> Karabiner-VirtualHIDDevice-SMAppServiceExample.app

--- scripts/clean-launch-services-database.swift
+++ scripts/clean-launch-services-database.swift
@@ -13,9 +13,9 @@ import AppKit
 
 let bundleIdentifiers = [
   "org.pqrs.HIDManagerTool",
-  "org.pqrs.Karabiner-DriverKit-SMAppServiceExample",
   "org.pqrs.Karabiner-VirtualHIDDevice-Daemon",
   "org.pqrs.Karabiner-VirtualHIDDevice-Manager",
+  "org.pqrs.Karabiner-VirtualHIDDevice-SMAppServiceExample",
   "org.pqrs.virtual-hid-device-service-client",
 ]
 
