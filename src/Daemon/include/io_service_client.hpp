1feb2dd4471dc8c2b70a43feacdcc33486f07f3e
Takayama Fumihiko <tekezo@pqrs.org>
2025-05-25 18:33:27 +0900

    Improved to maintain the active connection even when a new service is detected

--- src/Daemon/include/io_service_client.hpp
+++ src/Daemon/include/io_service_client.hpp
@@ -4,6 +4,7 @@
 #include "version.hpp"
 #include <IOKit/IOKitLib.h>
 #include <array>
+#include <gsl/gsl>
 #include <nod/nod.hpp>
 #include <optional>
 #include <os/log.h>
@@ -42,7 +43,9 @@ public:
                                __func__);
 
     detach_from_dispatcher([this] {
-      close_connection();
+      if (auto s = matched_services_.find_opened()) {
+        close_connection(s->get_registry_entry_id());
+      }
 
       service_monitor_ = nullptr;
     });
@@ -133,17 +136,31 @@ public:
           logger::get_logger()->info("{0} iokit_service_monitor::service_matched",
                                      virtual_hid_device_service_client_endpoint_filename_);
 
-          close_connection();
+          matched_services_.insert(registry_entry_id,
+                                   service_ptr);
 
-          // Use the last matched service.
-          open_connection(service_ptr);
+          logger::get_logger()->info("{0} matched_services_ size: {1}",
+                                     virtual_hid_device_service_client_endpoint_filename_,
+                                     matched_services_.get_services().size());
+
+          open_connection();
         });
 
         service_monitor_->service_terminated.connect([this](auto&& registry_entry_id) {
           logger::get_logger()->info("{0} iokit_service_monitor::service_terminated",
                                      virtual_hid_device_service_client_endpoint_filename_);
 
-          close_connection();
+          close_connection(registry_entry_id);
+
+          matched_services_.erase(registry_entry_id);
+
+          logger::get_logger()->info("{0} matched_services_ size: {1}",
+                                     virtual_hid_device_service_client_endpoint_filename_,
+                                     matched_services_.get_services().size());
+
+          // If the alive connection is closed by `close_connection`,
+          // we attempt to connect to the next available service.
+          open_connection();
         });
 
         service_monitor_->error_occurred.connect([this](auto&& message, auto&& kern_return) {
@@ -338,6 +355,101 @@ public:
   }
 
 private:
+  class matched_service final {
+  public:
+    matched_service(pqrs::osx::iokit_registry_entry_id::value_t registry_entry_id,
+                    pqrs::osx::iokit_object_ptr service)
+        : registry_entry_id_(registry_entry_id),
+          service_(service),
+          opened_(false),
+          invalidated_(false) {
+    }
+
+    pqrs::osx::iokit_registry_entry_id::value_t get_registry_entry_id(void) const {
+      return registry_entry_id_;
+    }
+
+    pqrs::osx::iokit_object_ptr get_service(void) const {
+      return service_;
+    }
+
+    bool get_opened(void) const {
+      return opened_;
+    }
+
+    void set_opened(bool value) {
+      opened_ = value;
+    }
+
+    bool get_invalidated(void) const {
+      return invalidated_;
+    }
+
+    void set_invalidated(bool value) {
+      invalidated_ = value;
+    }
+
+  private:
+    pqrs::osx::iokit_registry_entry_id::value_t registry_entry_id_;
+    pqrs::osx::iokit_object_ptr service_;
+    bool opened_;
+    // `invalidated_` is set to true if the driver version cannot be retrieved.
+    bool invalidated_;
+  };
+
+  class matched_services final {
+  public:
+    void insert(pqrs::osx::iokit_registry_entry_id::value_t registry_entry_id,
+                pqrs::osx::iokit_object_ptr service) {
+      if (std::ranges::any_of(services_,
+                              [registry_entry_id](const auto& s) {
+                                return s->get_registry_entry_id() == registry_entry_id;
+                              })) {
+        return;
+      }
+
+      services_.push_back(std::make_shared<matched_service>(registry_entry_id, service));
+    }
+
+    void erase(pqrs::osx::iokit_registry_entry_id::value_t registry_entry_id) {
+      std::erase_if(services_,
+                    [registry_entry_id](const auto& s) {
+                      return s->get_registry_entry_id() == registry_entry_id;
+                    });
+    }
+
+    const std::vector<gsl::not_null<std::shared_ptr<matched_service>>>& get_services(void) const {
+      return services_;
+    }
+
+    std::shared_ptr<matched_service> find(pqrs::osx::iokit_registry_entry_id::value_t registry_entry_id) const {
+      auto it = std::ranges::find_if(services_,
+                                     [registry_entry_id](const auto& s) {
+                                       return s->get_registry_entry_id() == registry_entry_id;
+                                     });
+      if (it == std::end(services_)) {
+        return nullptr;
+      }
+
+      return *it;
+    }
+
+    std::shared_ptr<matched_service> find_opened(void) const {
+      auto it = std::ranges::find_if(services_,
+                                     [](const auto& s) {
+                                       return s->get_opened();
+                                     });
+      if (it == std::end(services_)) {
+        return nullptr;
+      }
+
+      return *it;
+    }
+
+  private:
+    std::vector<gsl::not_null<std::shared_ptr<matched_service>>> services_;
+  };
+
   // This method is executed in the dispatcher thread.
   void set_driver_version(std::optional<pqrs::karabiner::driverkit::driver_version::value_t> value) {
     std::lock_guard<std::mutex> lock(driver_version_mutex_);
@@ -387,7 +499,7 @@ private:
   }
 
   // This method is executed in the dispatcher thread.
-  void open_connection(pqrs::osx::iokit_object_ptr s) {
+  void open_connection(void) {
     if (connection_) {
       return;
     }
@@ -396,57 +508,75 @@ private:
     set_virtual_hid_keyboard_ready(std::nullopt);
     set_virtual_hid_pointing_ready(std::nullopt);
 
-    service_ = s;
+    for (const auto& matched_service : matched_services_.get_services()) {
+      if (matched_service->get_invalidated()) {
+        continue;
+      }
 
-    io_connect_t c;
-    pqrs::osx::iokit_return r = IOServiceOpen(*service_, mach_task_self(), 0, &c);
+      io_connect_t c;
+      pqrs::osx::iokit_return r = IOServiceOpen(*(matched_service->get_service()),
+                                                mach_task_self(),
+                                                0,
+                                                &c);
 
-    if (!r) {
-      logger::get_logger()->error("{0} io_service_client IOServiceOpen error: {1}",
-                                  virtual_hid_device_service_client_endpoint_filename_,
-                                  r.to_string());
-      connection_.reset();
-      return;
-    }
+      if (!r) {
+        logger::get_logger()->error("{0} io_service_client IOServiceOpen error: {1}",
+                                    virtual_hid_device_service_client_endpoint_filename_,
+                                    r.to_string());
+        continue;
+      }
 
-    connection_ = pqrs::osx::iokit_object_ptr(c);
+      //
+      // Check driver version
+      //
 
-    //
-    // Check driver version
-    //
+      auto driver_version = call_driver_version(c);
+      set_driver_version(driver_version);
+      if (!driver_version) {
+        logger::get_logger()->error("{0} io_service_client failed to get driver_version",
+                                    virtual_hid_device_service_client_endpoint_filename_);
+        IOServiceClose(c);
+        matched_service->set_invalidated(true);
+        continue;
+      }
 
-    auto driver_version = call_driver_version();
-    set_driver_version(driver_version);
-    if (!driver_version) {
-      logger::get_logger()->error("{0} io_service_client failed to get driver_version",
-                                  virtual_hid_device_service_client_endpoint_filename_);
-      connection_.reset();
-      return;
-    }
-
-    enqueue_to_dispatcher([this] {
-      logger::get_logger()->info("{0} io_service_client::opened",
-                                 virtual_hid_device_service_client_endpoint_filename_);
-
-      opened();
-    });
-  }
-
-  // This method is executed in the dispatcher thread.
-  void close_connection(void) {
-    if (connection_) {
-      IOServiceClose(*connection_);
-      connection_.reset();
+      connection_ = pqrs::osx::iokit_object_ptr(c);
+      matched_service->set_opened(true);
 
       enqueue_to_dispatcher([this] {
-        logger::get_logger()->info("{0} io_service_client::closed",
+        logger::get_logger()->info("{0} io_service_client::opened",
                                    virtual_hid_device_service_client_endpoint_filename_);
 
-        closed();
+        opened();
       });
+
+      break;
+    }
+  }
+
+  // This method is executed in the dispatcher thread.
+  void close_connection(pqrs::osx::iokit_registry_entry_id::value_t registry_entry_id) {
+    // If no connection is open for the given registry_entry_id, do nothing.
+    auto matched_service = matched_services_.find(registry_entry_id);
+    if (!matched_service ||
+        !matched_service->get_opened()) {
+      return;
+    }
+    matched_service->set_opened(false);
+
+    if (!connection_) {
+      return;
     }
 
-    service_.reset();
+    IOServiceClose(*connection_);
+    connection_.reset();
+
+    enqueue_to_dispatcher([this] {
+      logger::get_logger()->info("{0} io_service_client::closed",
+                                 virtual_hid_device_service_client_endpoint_filename_);
+
+      closed();
+    });
 
     set_driver_version(std::nullopt);
     set_virtual_hid_keyboard_ready(std::nullopt);
@@ -454,8 +584,8 @@ private:
   }
 
   // This method is executed in the dispatcher thread.
-  std::optional<pqrs::karabiner::driverkit::driver_version::value_t> call_driver_version(void) const {
-    if (!connection_) {
+  std::optional<pqrs::karabiner::driverkit::driver_version::value_t> call_driver_version(io_connect_t connection) const {
+    if (!connection) {
       return std::nullopt;
     }
 
@@ -464,7 +594,7 @@ private:
 
     uint64_t output[1] = {0};
     uint32_t output_count = 1;
-    auto kr = IOConnectCallScalarMethod(*connection_,
+    auto kr = IOConnectCallScalarMethod(connection,
                                         static_cast<uint32_t>(pqrs::karabiner::driverkit::virtual_hid_device_driver::user_client_method::driver_version),
                                         nullptr,
                                         0,
@@ -570,7 +700,7 @@ private:
   std::string virtual_hid_device_service_client_endpoint_filename_;
   std::string service_name_;
   std::unique_ptr<pqrs::osx::iokit_service_monitor> service_monitor_;
-  pqrs::osx::iokit_object_ptr service_;
+  matched_services matched_services_;
   pqrs::osx::iokit_object_ptr connection_;
 
   mutable std::mutex driver_version_mutex_;
